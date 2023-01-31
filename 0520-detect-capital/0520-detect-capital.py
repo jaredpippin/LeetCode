2736415de1b94280cbbcd3ed503232d87441e52a
jaredpippin <jaredpippin19@gmail.com>
2023-01-30 16:28:32 -0800

    Time: 40 ms (30.47%), Space: 13.9 MB (48.59%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,10 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        count = 0
+        for i in word:
+            if (i.isupper()):
+                count+=1
+        if (count == 0 or count == len(word) or (count == 1 and word[0].isupper())):
+            return True
+        else:
+            return False
